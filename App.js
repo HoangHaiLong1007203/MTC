0f30efabaee085bdbc0dbbe650dfdc32bddafc77
HoangHaiLong1007203 <143704461+HoangHaiLong1007203@users.noreply.github.com>
2025-09-26 21:53:11 +0700

    Update App.js 3

--- App.js
+++ App.js
@@ -1,29 +1,42 @@
+import { View, StyleSheet, Dimensions } from "react-native";
 import AppNavigation from "./Navigation/Navigation";
 
 export default function App() {
+  const { width, height } = Dimensions.get("window");
+  const maxWidth = 430;
+
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: "#000",
-      }}
-    >
-      <div
-        style={{
-          height: "100%",             // cao = full màn
-          width: "min(100%, 430px)",  // tối đa 430px
-          background: "#fff",
-          boxShadow: "0 0 20px rgba(0,0,0,0.3)",
-          borderRadius: "12px",
-          overflow: "hidden",
-        }}
+    <View style={styles.outer}>
+      <View
+        style={[
+          styles.inner,
+          {
+            height: height,
+            width: width > maxWidth ? maxWidth : width, // nhỏ hơn hoặc bằng 430
+          },
+        ]}
       >
         <AppNavigation />
-      </div>
-    </div>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  outer: {
+    flex: 1,
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inner: {
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    overflow: "hidden",
+    elevation: 5, // shadow Android
+    shadowColor: "#000", // shadow iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+});
